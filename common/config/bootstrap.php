541b8e14958e168605c9f9a2b5847bec4095d394
lmk <894580582@qq.com>
2017-12-29 10:24:04 +0800

    个人博客

---
+++ common/config/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@pc', dirname(dirname(__DIR__)) . '/pc');
+Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
